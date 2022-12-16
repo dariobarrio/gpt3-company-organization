ff71da63268ff63d13358adaa03df3b933e5b21f
Dario Barrio <dbarrio@bodas.net>
2022-12-16 13:05:37 +0100

    feat: added the polite answerer for this app

--- pages/api/getPoliteAnswer.ts
+++ pages/api/getPoliteAnswer.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { Configuration, OpenAIApi } from "openai";
 
 type Data = string | undefined;
-type ListOfPeople = string;
+type SomeoneAskForSomething = string;
 
 const openAiConfiguration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -13,11 +13,11 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const listOfPeople = String(req.query.names);
+  const someoneAskForSomething = String(req.query.message);
 
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: generatePrompt(listOfPeople),
+    prompt: generatePrompt(someoneAskForSomething),
     max_tokens: 100,
     temperature: 0.2,
   });
@@ -26,8 +26,6 @@ export default async function handler(
   res.status(200).send(completion.data.choices[0].text);
 }
 
-function generatePrompt(listOfPeople: ListOfPeople) {
-  return `Assign random company positions to the following people: ${String(
-    listOfPeople
-  )}`;
+function generatePrompt(someoneAskForSomething: SomeoneAskForSomething) {
+  return `Return a short, polite and very comfortable response for this request "${someoneAskForSomething}" that states we'll working on it soon.`;
 }
