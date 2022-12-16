48cf73366b5337a74c328a1d3e9ac6e999378e17
Dario Barrio <dbarrio@bodas.net>
2022-12-16 12:37:28 +0100

    feat: now the api receives names via querystring

--- pages/api/getCrazyPositions.ts
+++ pages/api/getCrazyPositions.ts
@@ -1,9 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { uniqueNamesGenerator, Config, names } from "unique-names-generator";
-import americanSurnames from "../../data/surnames.json";
 import { Configuration, OpenAIApi } from "openai";
 
 type Data = any;
+type listOfPeople = string;
 
 const openAiConfiguration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -14,31 +13,21 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const config: Config = {
-    dictionaries: [names, americanSurnames],
-    separator: " ",
-  };
-
-  const listOfPeople = [];
-
-  for (let index = 0; index < 5; index++) {
-    listOfPeople.push(uniqueNamesGenerator(config));
-  }
+  console.log(req.query);
+  const listOfPeople = String(req.query.names);
 
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: generatePrompt(listOfPeople),
-    max_tokens: 200,
+    max_tokens: 100,
     temperature: 0.2,
   });
-
-  console.log(completion.data.choices);
 
   res.setHeader("Cache-Control", "no-store, max-age=0");
   res.status(200).send(completion.data.choices[0].text);
 }
 
-function generatePrompt(listOfPeople: Array<string>) {
+function generatePrompt(listOfPeople: string) {
   return `Assign random company positions to the following people: ${String(
     listOfPeople
   )}`;
