c112d74f2888873d6b4e2bf7a85d956c6d9237f4
Dario Barrio <dbarrio@bodas.net>
2022-12-16 12:21:16 +0100

    feat: added integration with GPT3 OpenIA

--- pages/api/getNames.ts
+++ pages/api/getNames.ts
@@ -1,10 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { uniqueNamesGenerator, Config, names } from "unique-names-generator";
 import americanSurnames from "../../data/surnames.json";
+import { Configuration, OpenAIApi } from "openai";
 
-type Data = Array<string>;
+type Data = any;
 
-export default function handler(
+const openAiConfiguration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(openAiConfiguration);
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
@@ -15,10 +21,25 @@ export default function handler(
 
   const listOfPeople = [];
 
-  for (let index = 0; index < 200; index++) {
+  for (let index = 0; index < 5; index++) {
     listOfPeople.push(uniqueNamesGenerator(config));
   }
 
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: generatePrompt(listOfPeople),
+    max_tokens: 200,
+    temperature: 0.2,
+  });
+
+  console.log(completion.data.choices);
+
   res.setHeader("Cache-Control", "no-store, max-age=0");
-  res.status(200).json(listOfPeople);
+  res.status(200).send(completion.data.choices[0].text);
+}
+
+function generatePrompt(listOfPeople: Array<string>) {
+  return `Assign random company positions to the following people: ${String(
+    listOfPeople
+  )}`;
 }
